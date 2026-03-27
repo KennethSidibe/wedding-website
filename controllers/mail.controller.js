ba05fb384789650cacc5a6828c8335194f19766b
KennethSidibe <Elstevehcks@gmail.com>
2026-03-27 18:39:21 -0400

    Add send email with validator

--- controllers/mail.controller.js
+++ controllers/mail.controller.js
@@ -1,34 +1,57 @@
 import nodemailer from "nodemailer";
-import { transporter } from "../database/connection.js";
+import { transporter, mailjet } from "../database/connection.js";
 import emailBody from "../constants/email.register.body.js";
-import { emailImgCid } from "../constants/string.constants.js";
+import { emailImgCid, websiteEmail } from "../constants/string.constants.js";
+import path, { resolve } from "path";
+import { validate } from "deep-email-validator";
+import { error } from "console";
+import fs from "fs";
 
-import path, {resolve} from "path";
-
-const imgDirPath = path.resolve('./img/');
-
+const imgDirPath = path.resolve('./public/img/');
 const imgDirPathFromModule = path.resolve('../public/img');
 
 console.log(imgDirPath);
-console.log(`From module: ${imgDirPathFromModule}`);
+console.log(`From module: ${imgDirPath}`);
 
-try {
-    const resp = await transporter.sendMail({
-        to :'sidibekenstevea@gmail.com',
-        subject: 'Merci de ta reservation au mariage!',
-        attachments: [{
-            filename: 'couple-3.webp',
-            path: `${imgDirPathFromModule}/webp/couple-3.webp`,
-            cid: emailImgCid
-        }],
-        html: emailBody
-    });
+const imgBuffer = fs.readFileSync(`${imgDirPath}/webp/couple-3.webp`);
+const imgBase64 = imgBuffer.toString("base64");
 
-    console.log(`Email send response`);
-    console.log(resp);
-    
-    
-} catch(error) {
-    console.log('Error while sending email');
-    console.error(error);
+async function sendEmail(recipient) {
+    let emailValid = await validate(recipient);
+    console.log('email Valid: ');
+
+    console.log(emailValid);
+
+    try {
+        if (emailValid.valid === true) {
+            const resp = await transporter.sendMail({
+                from: `"Légende et Sheridan" <${websiteEmail}>`,
+                to: recipient,
+                text: "Merci d'avoir RSVP",
+                subject: 'Merci de ta réservation au mariage!',
+                attachments: [{
+                    filename: 'couple-3.webp',
+                    path: `${imgDirPath}/webp/couple-3.webp`,
+                    cid: emailImgCid
+                }],
+                html: emailBody
+            });
+
+            console.log(`Email send response`);
+            console.log(resp);
+
+            return resp;
+        }
+        throw new Error('Email is invalid');
+    } catch (error) {
+        console.error(error);
+        throw error
+    }
 }
+
+// console.log('email resp: ');
+// console.log(await sendEmail('sidibestevekenus@gmail.com'));
+
+export {
+    sendEmail
+};
